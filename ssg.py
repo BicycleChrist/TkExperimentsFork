b466fe1446e3ac2af068fbce3bcf71d86415844d
DADMIN <139256274+DADMIN1@users.noreply.github.com>
2023-12-21 00:20:35 -0900

    Made 'CreateButton' and 'AddDropdown' member functions again.
    slightly improved focus handling

--- ssg.py
+++ ssg.py
@@ -11,6 +11,7 @@ from tkinter import ttk
 # The new entries unique to ttk are:
 # Combobox, Notebook, Progressbar, Separator, Sizegrip and Treeview
 
+# Extensions: "LabeledScale", "OptionMenu",
 
 GridAxisSelect = "Row"
 CurrentColumn = 0
@@ -44,17 +45,36 @@ def GetNextCoord(axis=None, ndelta=None):
 
 # standard skeleton of object-oriented tk-app
 class BaseApp(tkinter.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=None, title=None):
         super().__init__(master)
-        self.grid()
-        self.master.title("Epic Title")
+        self.grid()  # does nothing?
+        if title and master:
+            self.master.title(title)
 
-def AddDropdown(master, label, choices):
-    ncol, nrow = GetNextCoord()
-    choices.append("last choice")
-    newdropdown = ttk.Combobox(master, textvariable=tkinter.StringVar(), values=choices)
-    newdropdown.set(''.join((label,'(', str(ncol),',', str(nrow),')')))
-    newdropdown.grid(column=ncol, row=nrow, padx=10, pady=10)
+    # unfortunately combobox and listbox steal keyboard focus forever
+    # the only way to deselect is to hit tab
+    def AddDropdown(self, label, choices):
+        ncol, nrow = GetNextCoord()
+        choices.append("last choice")
+        newdropdown = ttk.Combobox(self, values=choices, takefocus=0)
+        newdropdown.set(''.join((label,'(', str(ncol),',', str(nrow),')')))
+        newdropdown.grid(column=ncol, row=nrow, padx=10, pady=10)
+
+    def AddListbox(self, choices):
+        ncol, nrow = GetNextCoord()
+        choices.append("last choice")
+        newvar = tkinter.StringVar()  # Listbox requires a stringvar
+        newvar.set(choices)  # why can't we do this in constructor
+        newlistbox = tkinter.Listbox(self, listvariable=newvar, selectmode="extended", exportselection=False)
+        newlistbox.grid(column=ncol, row=nrow, padx=10, pady=10)
+        # for some reason you can't update the list?
+
+    def CreateButton(self, text="default", command=None, nextcoord=None):
+        if not nextcoord:
+            nextcoord = GetNextCoord()
+        newbutton = tkinter.Button(self, text=text, command=command)
+        newbutton.grid(column=nextcoord[0], row=nextcoord[1], padx=10, pady=10)
+        newbutton.focus_set()
 
 def VariadicCallback(targetfunction, *args):
     #return lambda: targetfunction(*args)  # causes duplicates in CreateDropdown, for some reason
@@ -65,34 +85,19 @@ def Placeholder():
     print("pretending to do something")
 
 
-def CreateButton(parent, text="default", command=Placeholder, nextcoord=None):
-    if not nextcoord:
-        nextcoord = GetNextCoord()
-    newbutton = tkinter.Button(parent, text=text, command=command)
-    newbutton.grid(column=nextcoord[0], row=nextcoord[1], padx=10, pady=10)
-
-
 if __name__ == "__main__":
-    # root = tkinter.Tk(sync=True)  # 'sync' option causes X-server commands to be executed synchronously
-    root = tkinter.Tk()
-    window = BaseApp(root)
+    root = tkinter.Tk(sync=False)  # 'sync' option causes X-server commands to be executed synchronously
+    window = BaseApp(root, "Epic Title")
 
     # the call to VariadicCallback must be marked lambda, especially since it's not returning lambda anymore
-    newCB = lambda: VariadicCallback(AddDropdown, root, "new dropdown", ["choice 1", "Choice 2"])
+    newCB = lambda: VariadicCallback(window.AddDropdown, "new dropdown", ["choice 1", "Choice 2"])
     # if it returns lambda instead, then the dropdown menus get 'Last Choice' duplicated
     # it doesn't make a difference whether you '.copy()' the choices anywhere/everywhere
-    CreateButton(root, "more dropdown", command=newCB, nextcoord=(0,0))
+    window.CreateButton("more dropdown", command=newCB, nextcoord=(0,0))
     # this one-liner also works:
     #CreateButton(root, "create dropdown", command=lambda: VariadicCallback(AddDropdown, root, "new dropdown", ["choice 1", "Choice 2"]))
-    CreateButton(root, "more button", command=lambda: CreateButton(root, "new"))
-
-    newsvar = tkinter.StringVar()
-    newsvar.set("static string")
-    ttk.Combobox(root, textvariable=newsvar, values=["qwerty", "asdf"]).grid(padx=10, pady=10)
-    # you must create a seperate variable to hold the initial string; otherwise it shows up blank
-    # or you could call '.set' on the combobox itself, but then it must be named
-
-    AddDropdown(root, label="otherbox", choices=["createdby", "AddDropdown()"])
+    window.CreateButton("more button", command=lambda: window.CreateButton("new"))
+    window.AddDropdown(label="otherbox", choices=["createdby", "AddDropdown()"])
 
     def ArrowKeyCallback(event):
         keysymtable = { # axis, delta
@@ -102,11 +107,18 @@ if __name__ == "__main__":
             'Down':  ("Column", 1),
         }
         newcoord = GetNextCoord(*keysymtable[event.keysym])
-        CreateButton(root, event.keysym, command=Placeholder, nextcoord=newcoord)
+        window.CreateButton(event.keysym, command=Placeholder, nextcoord=newcoord)
+        event.widget.focus_set()  # try to break focus for ComboBox. Still doesn't work for down-arrow
+        #window.focus_set()  # also 'almost' works
 
     # assigning callbacks to each arrow key
     keycodes = [str('<' + ''.join(pair) + '>') for pair in zip(['Key-',]*4, ['Left','Right','Up','Down'])]
     for keycode in keycodes:
         root.bind(keycode, ArrowKeyCallback)
+
+    # cause mouseclicks to set focus
+    #root.bind_all("<Button-1>", lambda event: event.widget.focus_set())
+    # errors on ComboBox: 'str' object doesn't have attribute 'focus_set'
+    root.bind_all("<Button-1>", lambda event: window.focus_set())
 
     root.mainloop()
